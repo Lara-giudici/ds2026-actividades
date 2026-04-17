4535c5c71fcbdafad588949ab1fc7f60d1baa05d
Lara Giudici <lara.giudici@hotmail.com>
2026-04-17 13:48:23 -0300

    Ejercicio JS 1: variables y operaciones

---
+++ c03-javascript/ej1-variables-operaciones/app.js
@@ -0,0 +1,16 @@
+const nombre = "Lara Giudici"; 
+const edad = 22; 
+const materia = "Desarrollo de Software";
+
+
+console.log(`Me llamo ${nombre}, tengo ${edad} años y curso ${materia}`);
+
+
+let contador = 0;
+
+
+contador = contador + 1;
+contador = contador + 1;
+contador = contador + 1;
+
+console.log("Valor final:", contador);
