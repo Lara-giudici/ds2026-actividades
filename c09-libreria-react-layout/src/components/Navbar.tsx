e0ea7589106eebe89f2e417c9035bf3912c1f5f6
Lara Giudici <lara.giudici@hotmail.com>
2026-06-25 00:41:55 -0300

    Feat: agregar layout

--- c09-libreria-react-layout/src/components/Navbar.tsx
+++ c09-libreria-react-layout/src/components/Navbar.tsx
@@ -1,9 +1,29 @@
+import { Link } from "react-router-dom";
+
 function Navbar() {
   return (
     <nav className="navbar navbar-dark bg-dark px-4">
-      <span className="navbar-brand mb-0 h1">
+
+      <span className="navbar-brand">
         Librería React
       </span>
+
+      <div>
+        <Link
+          to="/"
+          className="text-white text-decoration-none me-3"
+        >
+          Inicio
+        </Link>
+
+        <Link
+          to="/catalogo"
+          className="text-white text-decoration-none"
+        >
+          Catálogo
+        </Link>
+      </div>
+
     </nav>
   );
 }
